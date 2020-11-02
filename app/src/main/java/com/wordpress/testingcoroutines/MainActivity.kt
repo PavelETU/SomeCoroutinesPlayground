052ace17ef987f919ae4e5c07b77f72ce3c08c81
PavelETU <pavelsuvit@gmail.com>
2020-11-02 18:30:18 +0000

    Display simple status

--- app/src/main/java/com/wordpress/testingcoroutines/MainActivity.kt
+++ app/src/main/java/com/wordpress/testingcoroutines/MainActivity.kt
@@ -2,15 +2,47 @@ package com.wordpress.testingcoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.lifecycle.coroutineScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import kotlin.coroutines.coroutineContext
 
+@ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val viewModel: ViewModelToUse by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val contextOfTheScreen = findViewById<TextView>(R.id.context_of_the_screen)
+        val loadingIndicator = findViewById<ProgressBar>(R.id.loading_indicator)
+        viewModel.start()
+        lifecycle.coroutineScope.launchWhenCreated {
+            viewModel.state.collect {
+                when(it) {
+                    StateOfScreen.SUCCESS -> {
+                        contextOfTheScreen.visibility = View.VISIBLE
+                        loadingIndicator.visibility = View.INVISIBLE
+                        contextOfTheScreen.text = getString(R.string.success)
+                    }
+                    StateOfScreen.LOADING -> {
+                        contextOfTheScreen.visibility = View.INVISIBLE
+                        loadingIndicator.visibility = View.VISIBLE
+                    }
+                    StateOfScreen.ERROR -> {
+                        contextOfTheScreen.visibility = View.VISIBLE
+                        loadingIndicator.visibility = View.INVISIBLE
+                        contextOfTheScreen.text = getString(R.string.error)
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/wordpress/testingcoroutines/RepositoryImpl.kt
+++ app/src/main/java/com/wordpress/testingcoroutines/RepositoryImpl.kt
@@ -1,10 +1,12 @@
 package com.wordpress.testingcoroutines
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(): Repository {
-    override suspend fun someLongRunningOperation() {
-        delay(1000)
+    override suspend fun someLongRunningOperation() = withContext(Dispatchers.IO) {
+        delay(2000)
     }
 }
